63e6a2c5ffb97f2e40a1cf46ea844642e08d8854
TruongLapPhuoc <phuocnhon55@gmail.com>
2022-07-14 11:14:48 +0700

    todolist

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/todolist/TodoList';
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+export default App;
